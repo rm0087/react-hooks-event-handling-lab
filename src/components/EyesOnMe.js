536d210000c55e1720bc7841206e23cc4f2a3528
rm0087 <michetti.ray@gmail.com>
2024-06-29 15:27:44 -0400

    hooks event handling lab complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+export default function EyesOnMe(){
+    const handleFocus = () => {console.log("Good!")}
+    const handleBlur = () => {console.log("Hey! Eyes on me!")};
+    
+    return(
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
+import React from 'react';
+import { useState } from 'react';
 
-function Keypad (){
+export default function Keypad (){
+    const [pass, setPass] = useState("");
+
+    const handleChange = (event)=>{setPass(event.target.value); console.log("Entering password...")};
+    
     return (
-        <div></div>
-    )
+        <div>
+            <input type="password" value={pass} onChange={handleChange} />
+        </div>
+    );
 }
-
-export default Keypad;
